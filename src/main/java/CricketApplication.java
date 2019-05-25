06d816d79c90e10427f227176e3282ec65e8ab85
Deepti <bndeepti@gmail.com>
2019-05-25 13:37:04 +0530

    Fix failing tests

--- src/main/java/CricketApplication.java
+++ src/main/java/CricketApplication.java
@@ -11,7 +11,7 @@ public class CricketApplication {
 
         Context context = tomcat.addContext("/cricket", new File(".").getAbsolutePath());
 
-        tomcat.addServlet(context, "Cricket", new CricketServlet());
+//        tomcat.addServlet(context, "Cricket", new CricketServlet());
         context.addServletMappingDecoded("/*", "Cricket");
 
         tomcat.start();
